1c1aacb5f7a89092b59d025e6635cbeb45e94b27
Satvir <sssatvir12@gmail.com>
2026-01-03 14:14:12 +0530

    adding controllers to text fields

--- todo_crud_app/lib/screens/add_list.dart
+++ todo_crud_app/lib/screens/add_list.dart
@@ -8,11 +8,42 @@ class AddTodo extends StatefulWidget {
 }
 
 class _AddTodoState extends State<AddTodo> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Add Todo 2')),
-      body: const Center(child: Text('This is the Add Todo Screen')),
+      appBar: AppBar(title: const Text('Add Todo')),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: const InputDecoration(
+              hintText: 'Enter Todo Title',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            controller: descriptionController,
+            decoration: const InputDecoration(
+              hintText: 'Enter Todo Description',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(onPressed: submitData, child: const Text('Save Todo')),
+        ],
+      ),
     );
+  }
+
+  void submitData() {
+    String title = titleController.text;
+    String description = descriptionController.text;
+    print(title);
+    print(description);
+    // Further processing like saving the data
   }
 }
