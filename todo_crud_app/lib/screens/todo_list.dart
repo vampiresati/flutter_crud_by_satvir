e5e0b1ff86d21b4656631227cdf29f3af798ca3c
Satvir <sssatvir12@gmail.com>
2026-01-03 13:59:08 +0530

    navigating to add page

--- todo_crud_app/lib/screens/todo_list.dart
+++ todo_crud_app/lib/screens/todo_list.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo_crud_app/screens/add_list.dart';
+
+import 'package:flutter/material.dart';
+import 'package:todo_crud_app/screens/add_list.dart';
 
 class TodoList extends StatefulWidget {
   const TodoList({super.key});
@@ -8,15 +12,24 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
+  // Navigation function has access to context
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todo List'),
+      appBar: AppBar(title: const Text('Todo List 2024')),
+      body: const Center(child: Text('This is the Todo List Screen')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: navigatetoaddpage, // Works now
+        child: const Icon(Icons.add),
       ),
-      body: const Center(
-        child: Text('This is the Todo List Screen'),
-      ),
+    );
+  }
+
+  void navigatetoaddpage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const AddTodo()),
     );
   }
 }
