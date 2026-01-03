5c6ec95604fa2111d82c6e7fe7eedf45f8a0e312
Satvir <sssatvir12@gmail.com>
2026-01-03 13:39:03 +0530

    making screen folder

---
+++ todo_crud_app/lib/screens/todo_list.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatefulWidget {
+  const TodoList({super.key});
+
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+      ),
+      body: const Center(
+        child: Text('This is the Todo List Screen'),
+      ),
+    );
+  }
+}
